27e93c31f6b57822db5b62412429352973647083
Lissa Ten <lissa10ten@gmail.com>
2019-04-25 18:29:10 +0300

    comparison if

--- hw19Apr.js
+++ hw19Apr.js
@@ -84,4 +84,29 @@ switch(Case) {
         myQ.mAge = ++myQ.mAge
         var newAge = alert(myQ.mAge)
         break;
-    
+    case "Comparison if":
+        var age = +prompt("Сколько вам лет?","");
+        if (age < 18){
+            alert("школьник");
+        }
+        else {
+            if (age < 30){
+            alert("молодеж");}            
+            else {
+                if (age < 45){
+                alert("зрелость");}
+                else {
+                    if (age < 60){
+                    alert("закат");}
+                    else {
+                        if (age > 60){
+                        alert("как пенсия?");}
+                        else {
+                            alert("то ли киборг, то ли ошибка"); 
+                        }
+                    }
+                }
+            }
+        }
+        break;
+        
